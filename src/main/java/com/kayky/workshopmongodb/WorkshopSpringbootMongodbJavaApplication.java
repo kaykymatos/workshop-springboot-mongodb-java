1891d1e99809144383f37d6fea101d7b2a9cdea3
Kayky Matos Santana <kayky7277@gmail.com>
2022-06-11 19:51:01 -0300

    initial commit

---
+++ src/main/java/com/kayky/workshopmongodb/WorkshopSpringbootMongodbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kayky.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kayky/workshopmongodb/WorkshopSpringbootMongodbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayky.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
