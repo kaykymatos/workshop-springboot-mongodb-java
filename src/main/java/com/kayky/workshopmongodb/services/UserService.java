e03918a6d653634c004c3039469cbd483ff5a540
Kayky Matos Santana <kayky7277@gmail.com>
2022-06-12 16:16:04 -0300

    Create, Read, Update, Delet Methods to User(CRUD)

--- src/main/java/com/kayky/workshopmongodb/services/UserService.java
+++ src/main/java/com/kayky/workshopmongodb/services/UserService.java
@@ -1,20 +1,52 @@
 package com.kayky.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kayky.workshopmongodb.domain.User;
+import com.kayky.workshopmongodb.dto.UserDTO;
 import com.kayky.workshopmongodb.repository.UserRepository;
+import com.kayky.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
-		return repository.findAll();	
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
